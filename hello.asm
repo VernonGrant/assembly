05f8029a7f4924e7189be22221b0f18cb497593c
Vernon Grant <vernon@ruppell.io>
2022-03-20 22:33:23 +0800

    Add comments to hello.asm

--- hello.asm
+++ hello.asm
@@ -1,21 +1,40 @@
 ;;; section .data
-;;; Initializes data in the format of:
+;;; Holds initialized variables in the format of:
 ;;; <variable_name> <type> <value>
 ;;;
-;;; Data types:
+;;; It can also hold constants using the equ type.
+;;;
+;;; Data types (d stands for data):
 ;;; db - Byte        - 8 bits
 ;;; dw - Word        - 16 bits
 ;;; dd - Double Word - 32 bits
 ;;; dq - Quadword    - 64 bits
 
+;;; section .bss
+;;; Stands for Block Started by Symbol. Holds space that will be used at
+;;; execution time, essentially empty memory blocks, in the format of:
+;;; <variable_name> <type> <number>
+;;;
+;;; Data types:
+;;; resb - Byte        - 8 bits
+;;; resw - Word        - 16 bits
+;;; resd - Double Word - 32 bits
+;;; resq - Quadword    - 64 bits
+
+;;; section .text
+;;; Contains the program code.
+;;; main: is called a label
+
 section .data
+	;; The ,0 is the string terminator. Marking the end of the string.
+	;; The variable name msg is a memory address that points to h.
 	msg    db      "hello, world",0
 section .bss
 section .text
 	global main
 main:
 	mov    rax, 1       ; 1 = write
-	mov    rdi, 1       ; 1 = to stdout
+	mov    rdi, 1       ; 1 = stdout
 	mov    rsi, msg     ; string to display in rsi
 	mov    rdx, 12      ; length of the string, without 0
 	syscall             ; display the string
